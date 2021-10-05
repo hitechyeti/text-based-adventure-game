debce27b949ed80402f6f8bf1229632d75087a9f
hitechyeti <53953660+hitechyeti@users.noreply.github.com>
2021-10-04 18:10:02 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,124 +1,195 @@
-function attacking (att, def){
-    console.log(att.proff + " attack is " + att.attack);
-    console.log(def.proff + " has " + def.hp + " hp");
-    let dodging = Math.floor(Math.random() * 100 + def.dodge);
-    console.log(dodging);
-    if (dodging >= 100){
-        console.log(def.proff + " has " + def.hp + " hp");
-        return def.proff + " dodged attack of " + att.proff;
-    } else {
-        def.hp = def.hp - att.attack;
-        if (def.hp < 0){
-            console.log(def.proff + " got hit by " + att.proff + " and lost " + att.attack + " hp");
-            return def.proff + " has 0 hp and is dead";
-        }
-        console.log(def.proff + " has " + def.hp + " hp");
-        return def.proff + " got hit by " + att.proff + " and lost " + att.attack + " hp \nNow he has " + def.hp + " hp";
-    }
+const textElement = document.getElementById('text')
+const optionButtonsElement = document.getElementById('option-buttons')
+
+let state = {}
+
+function startGame() {
+  state = {}
+  showTextNode(1)
+}
+
+function showTextNode(textNodeIndex) {
+  const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+  textElement.innerText = textNode.text
+  while (optionButtonsElement.firstChild) {
+    optionButtonsElement.removeChild(optionButtonsElement.firstChild)
   }
 
-var story;
-function getStory(name) {
-    return {
-        currentScene: "attack",
-        attack: {
-        title: "Chapter 1",
-        story: `Once up a time our game started YAY! ${name}`,
-        choices: [
-            {
-                choice: "Yes for choice one",
-                destination: 'battle'
-            },
-            {
-                choice: "Go Home",
-                destination: 'goHome'
-            }
-        ]
-        },
-        battle: {
-            title: 'Chapter 2',
-            story: 'its time to battle',
-            choices: [
-                {
-                    choice: "Attack with a sword yo!",
-                    destination: "sword"
-                },
-                {
-                    choice: "Attack with a caddlestick?",
-                    destination: "candlestick"
-                }
-            ]
-        },
-        goHome: {
-            title: "Go Home!",
-            story: "Wow you are home already?!?!",
-            defaultDestination: 'attack',
-            image: "picture.png",
-            buttonText: "Let's try again"
-        }
+  textNode.options.forEach(option => {
+    if (showOption(option)) {
+      const button = document.createElement('button')
+      button.innerText = option.text
+      button.classList.add('btn')
+      button.addEventListener('click', () => selectOption(option))
+      optionButtonsElement.appendChild(button)
     }
+  })
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    var button = document.querySelector('#start-button')
-    var content = document.querySelector('#content')
-    button.addEventListener('click', function() {
-        var input = document.querySelector('#name-input')
-        story = getStory(name.value)
-        renderScreen()
-    })
-})
-
-function renderScreen() {
-    var text = "Next"
-    var image = "";
-    if (story[story.currentScene].image) {
-        image = "<img></img>"
-    }
-    if (story[story.currentScene].buttonText) {
-        text = story[story.currentScene].buttonText
-    }
-    content.innerHTML = `
-    <h1>${story[story.currentScene].title}</h1>
-    <p>${story[story.currentScene].story}</p>
-    ${image}
-    ${getInputs()}
-    <button id = "submit-button">${text}</button>
-    `
-    if (story[story.currentScene].image) {
-        document.querySelector("img").src = `./img/${story[story.currentScene].image}`
-    }
-    var button = document.querySelector("#submit-button");
-    button.addEventListener('click', function() {
-        getInputValue()
-    })
+function showOption(option) {
+  return option.requiredState == null || option.requiredState(state)
 }
 
-function getInputValue() {
-    var inputs = document.querySelectorAll('input[type="radio"]');
-    for (var i = 0; i < inputs.length; i++) {
-        if (inputs[i].checked) {
-            story.currentScene = inputs[i].getAttribute('data-destination')
-            renderScreen();
-            return;
-        }
-    }
-    story.currentScene = story[story.currentScene].defaultDestination
-    renderScreen()
+function selectOption(option) {
+  const nextTextNodeId = option.nextText
+  if (nextTextNodeId <= 0) {
+    return startGame()
+  }
+  state = Object.assign(state, option.setState)
+  showTextNode(nextTextNodeId)
 }
 
-getInputs()
-function getInputs() {
-    var input = ""
-    if (!story[story.currentScene].choices){
-        return ""
-    }
-    for(var i = 0; i < story[story.currentScene].choices.length; i++) {
-        input += `
-        <div>
-            <input data-destination = ${story[story.currentScene].choices[i].destination} id = "radio${i}" type = "radio" name = "choices" />
-            <label for "radio${i}">${story[story.currentScene].choices[i].choice}</label>
-        </div>`
-    }
-    return input;
-}
+const textNodes = [
+  {
+    id: 1,
+    text: 'You wake up in a strange place and you see a jar of blue goo near you.',
+    options: [
+      {
+        text: 'Take the goo',
+        setState: { blueGoo: true },
+        nextText: 2
+      },
+      {
+        text: 'Leave the goo',
+        nextText: 2
+      }
+    ]
+  },
+  {
+    id: 2,
+    text: 'You venture forth in search of answers to where you are when you come across a merchant.',
+    options: [
+      {
+        text: 'Trade the goo for a sword',
+        requiredState: (currentState) => currentState.blueGoo,
+        setState: { blueGoo: false, sword: true },
+        nextText: 3
+      },
+      {
+        text: 'Trade the goo for a shield',
+        requiredState: (currentState) => currentState.blueGoo,
+        setState: { blueGoo: false, shield: true },
+        nextText: 3
+      },
+      {
+        text: 'Ignore the merchant',
+        nextText: 3
+      }
+    ]
+  },
+  {
+    id: 3,
+    text: 'After leaving the merchant you start to feel tired and stumble upon a small town next to a dangerous looking castle.',
+    options: [
+      {
+        text: 'Explore the castle',
+        nextText: 4
+      },
+      {
+        text: 'Find a room to sleep at in the town',
+        nextText: 5
+      },
+      {
+        text: 'Find some hay in a stable to sleep in',
+        nextText: 6
+      }
+    ]
+  },
+  {
+    id: 4,
+    text: 'You are so tired that you fall asleep while exploring the castle and are killed by some terrible monster in your sleep.',
+    options: [
+      {
+        text: 'Restart',
+        nextText: -1
+      }
+    ]
+  },
+  {
+    id: 5,
+    text: 'Without any money to buy a room you break into the nearest inn and fall asleep. After a few hours of sleep the owner of the inn finds you and has the town guard lock you in a cell.',
+    options: [
+      {
+        text: 'Restart',
+        nextText: -1
+      }
+    ]
+  },
+  {
+    id: 6,
+    text: 'You wake up well rested and full of energy ready to explore the nearby castle.',
+    options: [
+      {
+        text: 'Explore the castle',
+        nextText: 7
+      }
+    ]
+  },
+  {
+    id: 7,
+    text: 'While exploring the castle you come across a horrible monster in your path.',
+    options: [
+      {
+        text: 'Try to run',
+        nextText: 8
+      },
+      {
+        text: 'Attack it with your sword',
+        requiredState: (currentState) => currentState.sword,
+        nextText: 9
+      },
+      {
+        text: 'Hide behind your shield',
+        requiredState: (currentState) => currentState.shield,
+        nextText: 10
+      },
+      {
+        text: 'Throw the blue goo at it',
+        requiredState: (currentState) => currentState.blueGoo,
+        nextText: 11
+      }
+    ]
+  },
+  {
+    id: 8,
+    text: 'Your attempts to run are in vain and the monster easily catches.',
+    options: [
+      {
+        text: 'Restart',
+        nextText: -1
+      }
+    ]
+  },
+  {
+    id: 9,
+    text: 'You foolishly thought this monster could be slain with a single sword.',
+    options: [
+      {
+        text: 'Restart',
+        nextText: -1
+      }
+    ]
+  },
+  {
+    id: 10,
+    text: 'The monster laughed as you hid behind your shield and ate you.',
+    options: [
+      {
+        text: 'Restart',
+        nextText: -1
+      }
+    ]
+  },
+  {
+    id: 11,
+    text: 'You threw your jar of goo at the monster and it exploded. After the dust settled you saw the monster was destroyed. Seeing your victory you decide to claim this castle as your and live out the rest of your days there.',
+    options: [
+      {
+        text: 'Congratulations. Play Again.',
+        nextText: -1
+      }
+    ]
+  }
+]
+
+startGame()
